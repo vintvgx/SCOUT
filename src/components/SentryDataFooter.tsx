1613126651161605ded801f3bb59d2487310ad47
bandit.studios <banditstudiosdpd@gmail.com>
2024-05-18 11:38:18 -0400

    Updated styles for SentryDataFooter

--- src/components/SentryDataFooter.tsx
+++ src/components/SentryDataFooter.tsx
@@ -1,6 +1,6 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { SentryItem } from "../model/issue";
+import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import { useAppSelector } from "../redux/store";
 
 interface SentryDataFooterProps {
@@ -8,21 +8,29 @@ interface SentryDataFooterProps {
 }
 
 const SentryDataFooter: React.FC<SentryDataFooterProps> = ({ projectName }) => {
-  const { projects, loading, error, newIssues } = useAppSelector(
-    (state) => state.issues
-  );
-
-  const project = projects.find((p) => p.name === projectName); // Ensure the project is defined and has issues
-
-  let issues = project?.issues || [];
-  let errors = project?.errors || [];
+  const { projects, newIssues } = useAppSelector((state) => state.issues);
+  const project = projects.find((p) => p.name === projectName);
+  const issues = project?.issues || [];
+  const errors = project?.errors || [];
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.issueText}>Total Issues: {issues.length}</Text>
-      <Text style={styles.issueText}>Total Errors: {errors.length}</Text>
-      <Text style={styles.issueText}>New Issues: {newIssues.length}</Text>
-    </View>
+    <LinearGradient
+      colors={["#1E1E1E", "#202020"]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 0 }}
+      style={styles.container}>
+      <View style={styles.infoContainer}>
+        <Text style={styles.issueText}>
+          Issues: <Text style={styles.valueText}>{issues.length}</Text>
+        </Text>
+        <Text style={styles.issueText}>
+          Errors: <Text style={styles.valueText}>{errors.length}</Text>
+        </Text>
+        <Text style={styles.issueText}>
+          New Issues: <Text style={styles.valueText}>{newIssues.length}</Text>
+        </Text>
+      </View>
+    </LinearGradient>
   );
 };
 
@@ -30,14 +38,26 @@ export default SentryDataFooter;
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: "center",
+    height: 40,
+    paddingHorizontal: 16,
+    // borderRadius: 16,
+    overflow: "hidden",
+  },
+  infoContainer: {
+    flex: 1,
     justifyContent: "space-between",
     flexDirection: "row",
-    padding: 10,
-    backgroundColor: "#525252",
-    height: 40,
+    width: "100%",
+    alignContent: "center",
+    alignItems: "center",
   },
   issueText: {
-    color: "white",
+    color: "#B3B3B3",
+    fontSize: 14,
+    marginVertical: 2,
+  },
+  valueText: {
+    color: "#FFFFFF",
+    fontWeight: "bold",
   },
 });
